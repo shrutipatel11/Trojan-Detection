d6da814bc4f80197c91096875bc77b154197933b
Shruti Patel <43066790+shrutipatel11@users.noreply.github.com>
2020-06-15 15:27:51 +0530

    Compile all the benchmarks

---
+++ Benchmarks/compile.sh
@@ -0,0 +1,9 @@
+gcc fft.c -o fft -lm -w
+gcc angle.c -o angle -w
+gcc fir.c -o fir -w
+gcc ifft.c -o ifft -lm -w
+gcc matrix.c -o matrix -lm -w
+gcc road.c -o road -w
+gcc sink.c -o sink -w
+gcc src.c -o src -w
+gcc table.c -o table -w
