2499103c9c907d0bdadd3b3d86aff2e1ce3b2b4b
Shruti Patel <43066790+shrutipatel11@users.noreply.github.com>
2020-06-15 15:08:29 +0530

    Run
    Run this script to simulate HGC only mode

---
+++ HGC-only/run.sh
@@ -0,0 +1,16 @@
+taskgraph="mbench"
+n_hgc=3
+ip_hgc_scheduler="127.0.0.1"
+ip_hgc_slaves=("127.0.0.1" "127.0.0.1" "127.0.0.1")
+port_hgc_slaves=(10001 10002 10003 10004 10005)
+task_hgc_slaves=(30001 30002 30003 30004 30005)
+hgc_core=(1 2 3)
+output="Report_hgc_only"
+
+taskset -c 0 python hgc_scheduler.py $taskgraph $n_hgc "${ip_hgc_slaves[@]:0:$n_hgc}" "${port_hgc_slaves[@]:0:$n_hgc}" $output&
+sleep 2
+
+for (( i=0; i<$n_hgc; i++ ))
+do
+  taskset -c "${hgc_core[i]}" python hgc_slave.py $ip_hgc_scheduler "${port_hgc_slaves[i]}" "${task_hgc_slaves[$i]}" &
+done
